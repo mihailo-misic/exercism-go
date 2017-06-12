396398b4a06142d094fe969a57f4007b7c8382ef
mihailo <mihailo.misic@gmail.com>
2017-06-13 00:49:58 +0200

    Clock done.

--- clock/clock.go
+++ clock/clock.go
@@ -5,26 +5,42 @@ import "fmt"
 const testVersion = 4
 
 type Clock struct {
-    hour   int
-    minute int
+	hour   int
+	minute int
 }
 
 func New(hour, minute int) Clock {
-    hour += minute / 60  // Get extra hours from overflowing minutes
-    hour = hour % 24     // Get hours
-    minute = minute % 60 // Get minutes
+	// Round up hours if negative
+	if hour < 0 {
+		hour = 24 + (hour % 24)
+	}
 
-    return Clock{hour, minute}
+	hour += minute / 60 // Get extra hours from overflowing minutes
+
+	// Remove an extra hour if negative minutes are not exactly divisible by 60
+	if minute < 0 && minute%60 != 0 {
+		hour -= 1
+	}
+	// Round up hours if negative
+	if hour < 0 {
+		hour = 24 + (hour % 24)
+	}
+
+	hour = hour % 24     // Mold hours into 24 format with modulus
+	minute = minute % 60 // Mold minutes into 60 format with modulus
+
+	// Round up minutes if negative
+	if minute < 0 {
+		minute = 60 + minute
+	}
+
+	return Clock{hour, minute}
 }
 
 func (clock Clock) String() string {
-    return fmt.Sprintf("%02d:%02d", clock.hour, clock.minute)
+	return fmt.Sprintf("%02d:%02d", clock.hour, clock.minute)
 }
 
 func (clock Clock) Add(minutes int) Clock {
-    return New(clock.hour, clock.minute + minutes)
+	return New(clock.hour, clock.minute+minutes)
 }
-
-//func main() {
-//    fmt.Println(New(0,45).Add(40))
-//}
