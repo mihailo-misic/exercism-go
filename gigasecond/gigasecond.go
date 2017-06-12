f7d8071cbc4bf5d522b88c1ba49c17a6a55487af
mihailo <mihailo.misic@gmail.com>
2017-06-13 01:08:14 +0200

    ...

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,8 +1,9 @@
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
 const testVersion = 4
 
-func AddGigasecond(time.Time) time.Time
+func AddGigasecond(time.Time) time.Time {
+    return time.Now()
+}
