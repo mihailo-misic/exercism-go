4cf0fd152da9894ff454e11cb3c2ae61d8719873
mihailomisic <mihailo.misic@gmail.com>
2017-06-13 11:46:22 +0200

    Hamming done. Raindrops start.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,6 +1,23 @@
 package hamming
 
-const testVersion = 5
+import "errors"
+
+const testVersion = 6
 
 func Distance(a, b string) (int, error) {
+    var dif int
+
+    // Return error if they are different lengths
+    if len(a) != len(b) {
+        return -1, errors.New("DNAs are not the same length")
+    }
+
+    // Check letter by letter in DNAs
+    for i := range a {
+        if a[i] != b[i] {
+            dif++
+        }
+    }
+
+    return dif, nil
 }
