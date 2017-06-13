b8bbf160a03e9e8af42a17d5a0a466d24c19af45
mihailo <mihailo.misic@gmail.com>
2017-06-14 00:21:44 +0200

    Bob - done. Triangle - start.

--- bob/bob.go
+++ bob/bob.go
@@ -1,18 +1,32 @@
 package bob
 
+import (
+	"regexp"
+	"strings"
+)
+
 const testVersion = 3
 
 func Hey(s string) string {
+	r := ""
+	s = strings.TrimSpace(s)
+	yelling, _ := regexp.MatchString(`([A-Z]{4}|[A-Z]!)`, s)
+
+	if len(s) == 0 {
+		return "Fine. Be that way!"
+	} else if yelling {
+		return "Whoa, chill out!"
+	}
+
 	l := string(s[len(s)-1])
 
-	switch l {
-	case "?":
-		return "Sure."
-	case "!":
-		return "Whoa, chill out!"
-	case "":
-		return "Sure."
-	default:
-		return "Whatever."
+	if l == "?" {
+		r = "Sure."
+	} else if l == "\t" {
+		r = "Fine. Be that way!"
+	} else {
+		r = "Whatever."
 	}
+
+	return r
 }
