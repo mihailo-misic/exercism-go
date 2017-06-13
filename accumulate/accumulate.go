a1a85590517a7ba060440ffd84db5294f6a8d267
mihailomisic <mihailo.misic@gmail.com>
2017-06-13 17:35:26 +0200

    ...

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -6,8 +6,7 @@ func Accumulate(vals []string, funk func(string) string) []string {
 	var r []string
 
 	for _, v := range vals {
-		c := funk(v)
-		r = append(r, c)
+		r = append(r, funk(v))
 	}
 
 	return r
