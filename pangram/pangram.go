10e37f94e524d6c1f50664f6527a3d0ed7494f86
mihailomisic <mihailo.misic@gmail.com>
2017-06-13 18:38:01 +0200

    Pangram - done. Bob - start.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,21 @@
 package pangram
 
+import (
+	"strings"
+)
+
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	alph := "abcdefghijklmnopqrstuvwxyz"
+
+	for _, l := range s {
+		alph = strings.Replace(alph, strings.ToLower(string(l)), "", -1)
+	}
+
+	return len(alph) == 0
+}
