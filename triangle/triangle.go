ad757695374256cda60e7fabbd3b42a113c14827
mihailo <mihailo.misic@gmail.com>
2017-06-14 00:40:25 +0200

    Bob - done. Triangle - start.

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -2,15 +2,15 @@ package triangle
 
 const testVersion = 3
 
-func KindFromSides(a, b, c float64) Kind
+func KindFromSides(a, b, c float64) Kind {
+
+}
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 // Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
-
-// Organize your code for readability.
+var NaT = 0 // not a triangle
+var Equ = 1 // equilateral
+var Iso = 2 // isosceles
+var Sca = 3 // scalene
