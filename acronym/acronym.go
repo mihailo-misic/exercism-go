8c77fbe275f3068187dc41ef9ea57ae743cc988b
mihailomisic <mihailo.misic@gmail.com>
2017-06-13 18:02:13 +0200

    Acronym - done. Pangram - start.

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,5 +1,21 @@
 package acronym
 
-const testVersion = 2
+import (
+	"regexp"
+	"strings"
+)
 
-func Abbreviate(string) string
+const testVersion = 3
+
+func Abbreviate(s string) string {
+	r := ""
+	re := regexp.MustCompile(`\w+`) // I'm interested in words only
+
+	split := re.FindAllString(s, -1) // Split the words up
+
+	for _, v := range split {
+		r += strings.ToUpper(string(v[0])) // Append the UC first letters to result
+	}
+
+	return r
+}
