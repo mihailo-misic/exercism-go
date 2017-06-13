d1850ee1ce8e83ed3b5b4aa601d54236e0a23bd1
mihailomisic <mihailo.misic@gmail.com>
2017-06-13 19:14:13 +0200

    WIP Bob

--- bob/bob.go
+++ bob/bob.go
@@ -1,3 +1,18 @@
 package bob
 
 const testVersion = 3
+
+func Hey(s string) string {
+	l := string(s[len(s)-1])
+
+	switch l {
+	case "?":
+		return "Sure."
+	case "!":
+		return "Whoa, chill out!"
+	case "":
+		return "Sure."
+	default:
+		return "Whatever."
+	}
+}
