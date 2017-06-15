d619ded8695aa7794fdafeab9c63b3a148fd671e
mihailomisic <mihailo.misic@gmail.com>
2017-06-15 15:06:58 +0200

    twelve-days - done. difference-of-squares - start.

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -12,20 +12,20 @@ var Iso Kind = 1  // 2 same
 var Equ Kind = 3  // 3 same
 
 func KindFromSides(a, b, c float64) Kind {
-	var r Kind = 0
+	var r Kind
 
 	// Check if not a triangle
-	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) { // some side is NaN
-		return -1
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) { // Some side is NaN
+		return NaT
 	}
-	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) { // some side is NaN
-		return -1
+	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) { // Some side is NaN
+		return NaT
 	}
-	if a <= 0 || b <= 0 || c <= 0 { // no side can be <= 0
-		return -1
+	if a <= 0 || b <= 0 || c <= 0 { // No side can be <= 0
+		return NaT
 	}
-	if a+b < c || b+c < a || c+a < b { // fails triangle inequity
-		return -1
+	if a+b < c || b+c < a || c+a < b { // Fails triangle inequity
+		return NaT
 	}
 
 	// Count number of same sides
