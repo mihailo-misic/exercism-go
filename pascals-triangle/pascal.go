cd6dbf04f18a7a0d723534cb088becc3c73f1d6d
mihailomisic <mihailo.misic@gmail.com>
2017-06-18 16:44:38 +0200

    pascals-triangle (done). series (start).

--- pascals-triangle/pascal.go
+++ pascals-triangle/pascal.go
@@ -3,7 +3,19 @@ package pascal
 const testVersion = 1
 
 func Triangle(i int) [][]int {
-	var r [][]int
+	var r = make([][]int, i)
+
+	for x := 0; x < i; x++ {
+		r[x] = []int{}
+
+		for y := 0; y <= x; y++ {
+			if y == 0 || x == y {
+				r[x] = append(r[x], 1)
+			} else {
+				r[x] = append(r[x], r[x-1][y]+r[x-1][y-1])
+			}
+		}
+	}
 
 	return r
 }
