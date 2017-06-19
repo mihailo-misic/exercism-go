930745a72375d6aee7d49ef0864b094a57fdb3be
mihailo <mihailo.misic@gmail.com>
2017-06-19 23:04:01 +0200

    scrabble - done. parallel-letter-frequency - start.

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -1,5 +1,9 @@
 package letter
 
+import "fmt"
+
+const testVersion = 1
+
 type FreqMap map[rune]int
 
 func Frequency(s string) FreqMap {
@@ -8,4 +12,18 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+func ConcurrentFrequency(text []string) FreqMap {
+	c1 := make(chan FreqMap)
+	c2 := make(chan FreqMap)
+	c3 := make(chan FreqMap)
+	for i := 0; i < len(text); i++ {
+		go Frequency(text[i])
+	}
+	n1 := <-c1
+	n2 := <-c2
+	n3 := <-c3
+	fmt.Println(n1, n2, n3)
+	return n1 + n2 + n3
 }
