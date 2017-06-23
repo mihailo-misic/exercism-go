9c24c1c7b21ea019f556d8397ebe0db4345cf88d
mihailomisic <mihailo.misic@gmail.com>
2017-06-23 10:57:26 +0200

    sum_multiples improved.

--- sum-of-multiples/sum_multiples.go
+++ sum-of-multiples/sum_multiples.go
@@ -2,29 +2,17 @@ package summultiples
 
 const testVersion = 2
 
-// TODO Use a map instead of slice to store the used ints
 func SumMultiples(lim int, ints ...int) int {
-	r := 0
+	var r int
 
 	for i := 0; i < lim; i++ {
-		used := []int{}
-
 		for _, v := range ints {
-			if i%v == 0 && !intInSlice(i, used) {
+			if i%v == 0 {
 				r += i
-				used = append(used, i)
+				break
 			}
 		}
 	}
 
 	return r
-}
-
-func intInSlice(a int, list []int) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
 }
