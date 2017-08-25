79beaa74e460cffca90369f8121314c7993a2742
mihailomisic <mihailo.misic@gmail.com>
2017-08-26 00:12:04 +0200

    WIP: pythagorean-triplet.

--- pythagorean-triplet/pythagorean.go
+++ pythagorean-triplet/pythagorean.go
@@ -4,10 +4,26 @@ const testVersion = 1
 
 type Triplet [3]int
 
-func Range(min, max int) []Triplet {
+func Range(min, max int) (r []Triplet) {
+	for min <= max {
+		if min%3 == 0 {
+			m := min / 3
 
+			if 5*m > max {
+				break
+			}
+
+			r = append(r, Triplet{3 * m, 4 * m, 5 * m})
+			min += 3
+		} else {
+			min++
+		}
+	}
+
+	return r
 }
 
-func Sum(i int) []Triplet {
+func Sum(p int) (r []Triplet) {
 
+	return r
 }
